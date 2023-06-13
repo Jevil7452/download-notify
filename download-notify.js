4e06f503cc66e440c0b80768c896a0b47dedc14f
Jevil7452 <98893064+Jevil7452@users.noreply.github.com>
2023-06-13 23:15:37 +0200

    Notification on only download completion

--- download-notify.js
+++ download-notify.js
@@ -1,16 +1,5 @@
 var downloads = {};
 
-browser.downloads.onCreated.addListener(function reportDownload(item) {
-    browser.notifications.create({
-        "type": "basic",
-        "iconUrl": browser.extension.getURL("icons/icon-48.png"),
-        "title": "Download Started",
-        "message": item.filename
-    });
-
-    downloads[item.id] = item.filename;
-});
-
 browser.downloads.onChanged.addListener(function reportDownloadEnd(item) {
     if (item.state.current == "complete") {
         browser.notifications.create({
